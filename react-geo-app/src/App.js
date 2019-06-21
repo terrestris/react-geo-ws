b6d17836c06b856ff6cb29b00074901e12d19dea
Daniel Koch <koch@terrestris.de>
2019-06-21 09:37:25 +0200

    Update example app to match current workshop content

--- react-geo-app/src/App.js
+++ react-geo-app/src/App.js
@@ -9,17 +9,53 @@ import OlMap from 'ol/Map';
 import OlView from 'ol/View';
 import OlLayerTile from 'ol/layer/Tile';
 import OlSourceOsm from 'ol/source/OSM';
+import OlSourceTileJson from 'ol/source/TileJSON';
+import OlLayerGroup from 'ol/layer/Group';
 
+import { Drawer } from 'antd';
 import {
+  SimpleButton,
   MapComponent,
   NominatimSearch,
   MeasureButton,
-  Titlebar
+  LayerTree,
+  MapProvider,
+  mappify,
+  onDropAware
 } from '@terrestris/react-geo';
 
+const MappifiedNominatimSearch = mappify(NominatimSearch);
+const MappifiedMeasureButton = mappify(MeasureButton);
+const MappifiedLayerTree = mappify(LayerTree);
+const Map = mappify(onDropAware(MapComponent));
+
 const layer = new OlLayerTile({
-  name: 'OSM',
-  source: new OlSourceOsm()
+  source: new OlSourceOsm(),
+  name: 'OSM'
+});
+
+const layerGroup = new OlLayerGroup({
+  name: 'Layergroup',
+  layers: [
+    new OlLayerTile({
+      name: 'Food insecurity layer',
+      minResolution: 200,
+      maxResolution: 2000,
+      source: new OlSourceTileJson({
+        url: 'https://api.tiles.mapbox.com/v3/mapbox.20110804-hoa-foodinsecurity-3month.json?secure',
+        crossOrigin: 'anonymous'
+      })
+    }),
+    new OlLayerTile({
+      name: 'World borders layer',
+      minResolution: 2000,
+      maxResolution: 20000,
+      source: new OlSourceTileJson({
+        url: 'https://api.tiles.mapbox.com/v3/mapbox.world-borders-light.json?secure',
+        crossOrigin: 'anonymous'
+      })
+    })
+  ]
 });
 
 const center = [ 788453.4890155146, 6573085.729161344 ];
@@ -29,39 +65,55 @@ const map = new OlMap({
     center: center,
     zoom: 16,
   }),
-  layers: [layer]
+  layers: [layer, layerGroup]
 });
 
+map.on('postcompose', map.updateSize);
+
 class App extends Component {
+  state = {visible: false};
+
+  toggleDrawer = () => {
+    this.setState({visible: !this.state.visible});
+  }
+
   render() {
     return (
       <div className="App">
-        <Titlebar
-          className="titlebar"
-          tools={[
-            <NominatimSearch
+        <MapProvider map={map}>
+          <Map/>
+          <Drawer
+            title="react-geo-application"
+            placement="right"
+            onClose={this.toggleDrawer}
+            visible={this.state.visible}
+            mask={false}
+          >
+            <MappifiedNominatimSearch
               key="search"
-              map={map}
-            />,
-            <MeasureButton
+            />
+            <MappifiedMeasureButton
               key="measureButton"
               name="line"
-              map={map}
               measureType="line"
               icon="pencil"
             >
               Strecke messen
-            </MeasureButton>
-          ]}
-        >
-          react-geo-app
-        </Titlebar>
-        <MapComponent
-          map={map}
-        />
+            </MappifiedMeasureButton>
+            <MappifiedLayerTree
+              layerGroup={layerGroup}
+            />
+          </Drawer>
+          <SimpleButton
+            style={{position: 'fixed', top: '30px', right: '30px'}}
+            onClick={this.toggleDrawer}
+            icon="bars"
+          />
+        </MapProvider>
       </div>
     );
   }
 }
 
 export default App;
+
